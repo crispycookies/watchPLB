e457fe276fb94090a5479c1ff2cefed96e2722f5
paul70078 <paul70079@gmail.com>
2018-06-06 16:23:22 +0200

    enable dma clock and USART interrupt

--- Firmware/Drivers/User/uart/uart.c
+++ Firmware/Drivers/User/uart/uart.c
@@ -67,17 +67,25 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 		}
 	}
 
+	__HAL_RCC_DMA1_CLK_ENABLE();
+
+	uint8_t irq = 0;
+
 	if (conf->uart == USART1) {
 		__HAL_RCC_USART1_CLK_ENABLE();
+		irq = USART1_IRQn;
 		instances[UART_1] = inst;
 	} else if (conf->uart == USART2) {
 		__HAL_RCC_USART2_CLK_ENABLE();
+		irq = USART2_IRQn;
 		instances[UART_2] = inst;
 	} else if (conf->uart == USART4) {
 		__HAL_RCC_USART4_CLK_ENABLE();
+		irq = USART4_5_IRQn;
 		instances[UART_4] = inst;
 	} else if (conf->uart == USART5) {
 		__HAL_RCC_USART5_CLK_ENABLE();
+		irq = USART4_5_IRQn;
 		instances[UART_5] = inst;
 	}
 
@@ -128,6 +136,16 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 	HAL_DMA_Init(&(inst->rxDma));
 	__HAL_LINKDMA((&inst->uart), hdmarx, inst->rxDma);
 	DMA_RegisterInterrupt(&(inst->txDma));
+
+    HAL_NVIC_EnableIRQ(irq);
+
+	inst->rxCircHead = 0;
+	inst->rxCircTail = 0;
+	inst->rxCircWrap = FALSE;
+
+	inst->txCircHead = 0;
+	inst->txCircTail = 0;
+	inst->txCircWrap = FALSE;
 
 	startReceive(inst);
 }
