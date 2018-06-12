56a2385124ebdb09918be91cbcc73d571f133216
Unknown <egtobi@gmail.com>
2018-06-12 14:44:45 +0200

    Doxygen Documentation
    Finalized Doxygen Documentation for Vibrator/forceFeedback

--- Firmware/Drivers/User/forceFeedback/vibrator_driver.c
+++ Firmware/Drivers/User/forceFeedback/vibrator_driver.c
@@ -1,21 +1,29 @@
-/*
- * vibratordriver.c
- *
- *  Created on: 22.05.2018
- *      Author: tobilinux
- */
+/**
+  ******************************************************************************
+  * @file    vibrator_driver.c
+  * @author  Tobias Egger
+  * @version V1.1
+  * @date    12-June-2018
+  * @brief   WatchPLB Vibrator- Driver Implementation File
+  ******************************************************************************
+  */
+
 
 #include "vibrator_driver.h"
 
-#define true 1
-#define false 0
-
-// Global Init Variables for LED
-GPIO_InitTypeDef Vibrator_Oli;
+/* Global Init Variables for LED*/
+GPIO_InitTypeDef vibrator;
 GPIO_TypeDef  *bank;
 GPIO_PinType pin;
 
-uint8_t vibrator_init(GPIO_TypeDef BANK, GPIO_PinType PIN){
+
+/**
+  * @brief Initialize GPIOs for Vibrator
+  * @param  BANK: The GPIO Bank to use
+  * @param 	PIN: The Pin to use
+  * @retval Returns True if Params are Valid, false if not
+*/
+bool vibrator_init(GPIO_TypeDef BANK, GPIO_PinType PIN){
 	bank = bank;
 	pin = pin;
 
@@ -32,33 +40,50 @@ uint8_t vibrator_init(GPIO_TypeDef BANK, GPIO_PinType PIN){
 	}
 
 	//General Init
-	Vibrator_Oli.Mode = GPIO_MODE_OUTPUT_PP;
-	Vibrator_Oli.Pull = GPIO_PULLDOWN;
-	Vibrator_Oli.Speed = GPIO_SPEED_FREQ_LOW;
-	Vibrator_Oli.Pin |= PIN;
-	HAL_GPIO_Init(bank, &Vibrator_Oli);
+	vibrator.Mode = GPIO_MODE_OUTPUT_PP;
+	vibrator.Pull = GPIO_PULLDOWN;
+	vibrator.Speed = GPIO_SPEED_FREQ_LOW;
+	vibrator.Pin |= PIN;
+	HAL_GPIO_Init(bank, &vibrator);
 
 	return true;
 }
 
+/**
+  * @brief Turns on Vibrator
+  * @param  None
+  * @retval None
+*/
 void vibrator_on(){
 	HAL_GPIO_WritePin(bank, pin,  GPIO_PIN_SET);
 }
 
+/**
+  * @brief Turns off Vibrator
+  * @param  None
+  * @retval None
+*/
 void vibrator_off(){
 	HAL_GPIO_WritePin(bank, pin,  GPIO_PIN_RESET);
 }
 
+/**
+  * @brief Deinitializes Vibrator
+  * @warning Do not call if you are unsure
+  * @param  None
+  * @retval None
+*/
 void vibrator_deinit(){
+#warning Do not call if you are unsure
 	HAL_GPIO_DeInit(bank, pin);
 
 	if(bank == GPIOA){
-		__HAL_RCC_GPIOA_CLK_ENABLE();
+		__HAL_RCC_GPIOA_CLK_DISABLE();
 	}else if(bank == GPIOB){
-		__HAL_RCC_GPIOB_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_DISABLE();
 	}else if(bank == GPIOC){
-		__HAL_RCC_GPIOC_CLK_ENABLE();
+		__HAL_RCC_GPIOC_CLK_DISABLE();
 	}else if(bank == GPIOD){
-		__HAL_RCC_GPIOD_CLK_ENABLE();
+		__HAL_RCC_GPIOD_CLK_DISABLE();
 	}
 }
