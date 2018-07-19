2e020458ee10e22db6871a10dd36215d366a116e
Paul Goetzinger <paul70079@gmail.com>
2018-07-19 14:30:50 +0200

    added comments and additional checks against null- pointer

--- Firmware/Drivers/User/uart/uart.c
+++ Firmware/Drivers/User/uart/uart.c
@@ -22,24 +22,77 @@ typedef enum {
 	UART_5 = 3
 } UART_Modules;
 
+/**
+ * @brief Receive complete callback function
+ * 
+ * @param uart uart handle
+ */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *uart);
+
+/**
+ * @brief DMA receive abort callback function
+ * 
+ * @param dma dma handle
+ */
 void UART_DMARxOnlyAbortCallback(DMA_HandleTypeDef *dma);
+
+/**
+ * @brief Transmit complete callback function
+ * 
+ * @param uart uart handle
+ */
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *uart);
 
+/**
+ * @brief USART 1 interrupt handler
+ * 
+ */
 void USART1_IRQHandler();
+
+/**
+ * @brief USART 2 interrupt handler
+ * 
+ */
 void USART2_IRQHandler();
+
+/**
+ * @brief USART 4 and 5 interrupt handler
+ * 
+ */
 void USART4_5_IRQHandler();
 
+/**
+ * @brief start transmission
+ * 
+ * @param inst UART instance
+ */
 static void startTransmit(UART_Instance* inst);
+
+/**
+ * @brief start reception
+ * 
+ * @param inst UART instance
+ */
 static void startReceive(UART_Instance* inst);
 
-static UART_Instance* instances[UART_MODULE_COUNT];
+static uint8_t init = FALSE;	//module init flag
+static UART_Instance* instances[UART_MODULE_COUNT];	//uart instances table (needed for interrupts)
 
 void UART_Init(UART_Instance* inst, UART_Config* conf) {
-	for (uint8_t i = 0; i < UART_MODULE_COUNT; i++) {
-		instances[i] = 0;
+	if (!init) {
+		//if not already initialized clear instance table
+		for (uint8_t i = 0; i < UART_MODULE_COUNT; i++) {
+			instances[i] = 0;
+		}
+		init = TRUE;
 	}
 
+	//no valid parameter given
+	if ((!inst) || (!conf)) {
+		return;
+	}
+
+	//reset HAL handler
 	memset(&(inst->txDma), 0, sizeof(DMA_HandleTypeDef));
 	memset(&(inst->rxDma), 0, sizeof(DMA_HandleTypeDef));
 	memset(&(inst->uart), 0, sizeof(UART_HandleTypeDef));
@@ -67,12 +120,14 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 		}
 	}
 
+	//enable dma clock
 	__HAL_RCC_DMA1_CLK_ENABLE();
 
 	uint8_t irq = 0;
 	uint32_t txRequest = 0;
 	uint32_t rxRequest = 0;
 
+	//enable uart clock and retrieve dma request
 	if (conf->uart == USART1) {
 		__HAL_RCC_USART1_CLK_ENABLE();
 		irq = USART1_IRQn;
@@ -124,10 +179,12 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 	gpio.Alternate = conf->rxAF;
 	HAL_GPIO_Init(conf->rxBoard, &gpio);
 
+	//config tx pin
 	gpio.Pin = conf->txPin;
 	gpio.Alternate = conf->txAF;
 	HAL_GPIO_Init(conf->txBoard, &gpio);
 
+	//configure HAL uart module
 	inst->uart.Instance = conf->uart;
 	inst->uart.Init.BaudRate = conf->baud;
 	inst->uart.Init.HwFlowCtl = UART_HWCONTROL_NONE;
@@ -140,7 +197,7 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 
 	HAL_UART_Init(&(inst->uart));
 
-	//TX
+	//configure DMA for transmission
 	inst->txDma.Instance = conf->txDmaChannel;
 	inst->txDma.Init.Direction = DMA_MEMORY_TO_PERIPH;
 	inst->txDma.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
@@ -153,7 +210,7 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 	__HAL_LINKDMA(&(inst->uart), hdmatx, inst->txDma);
 	DMA_RegisterInterrupt(&(inst->txDma));
 
-	//RX
+	//configure DMA for reception
 	inst->rxDma.Instance = conf->rxDmaChannel;
 	inst->rxDma.Init.Direction = DMA_PERIPH_TO_MEMORY;
 	inst->rxDma.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
@@ -166,36 +223,58 @@ void UART_Init(UART_Instance* inst, UART_Config* conf) {
 	__HAL_LINKDMA(&(inst->uart), hdmarx, inst->rxDma);
 	DMA_RegisterInterrupt(&(inst->rxDma));
 
+	//enable uart interrupt
     HAL_NVIC_EnableIRQ(irq);
 
+	//reset receive buffer
 	inst->rxCircHead = 0;
 	inst->rxCircTail = 0;
 	inst->rxCircWrap = FALSE;
 
+	//reset transmit buffer
 	inst->txCircHead = 0;
 	inst->txCircTail = 0;
 	inst->txCircWrap = FALSE;
 
+	//start reception
 	startReceive(inst);
 }
 
 uint8_t UART_SendByte(UART_Instance* inst, uint8_t byte) {
+	//invalid parameter
+	if (!inst) {
+		return FALSE;
+	}
+
+	//return false on full buffer
 	if (inst->txCircWrap && inst->txCircHead == inst->txCircTail) {
 		return FALSE;
 	}
+
+	//load byte into buffer
 	inst->txCircBuf[inst->txCircHead++] = byte;
+
+	//if end of buffer reached wrap head
 	if (inst->txCircHead >= UART_TXBUFFER_SIZE) {
 		inst->txCircHead = 0;
 		inst->txCircWrap = TRUE;
 	}
 
+	//start transmission
 	startTransmit(inst);
 	return TRUE;
 }
 
 uint8_t UART_SendData(UART_Instance* inst, uint16_t len, uint8_t *data) {
+	//invalid parameter
+	if ((!inst) || (!data) || (!len)) {
+		return FALSE;
+	}
+
+	//return false if length of data is larger than the actual size of the buffer
 	if (len > UART_TXBUFFER_SIZE) {
 		return FALSE;
+	//return false if length of data is larger than the available space
 	} else if (inst->txCircWrap) {
 		if ((inst->txCircTail - inst->txCircHead) < len) {
 			return FALSE;
@@ -203,14 +282,21 @@ uint8_t UART_SendData(UART_Instance* inst, uint16_t len, uint8_t *data) {
 	} else if ((UART_TXBUFFER_SIZE - inst->txCircTail + inst->txCircHead) < len) {
 		return FALSE;
 	}
+
+	//check if wrap is needed
 	if (len > (UART_TXBUFFER_SIZE - inst->txCircHead)) {
 		inst->txCircWrap = TRUE;
 	}
+
+	//copy data in buffer
 	for (uint16_t i = 0; i < len; i++) {
 		inst->txCircBuf[(inst->txCircHead+i)%UART_TXBUFFER_SIZE] = data[i];
 	}
+
+	//adjust buffer head position
 	inst->txCircHead = (inst->txCircHead+len)%UART_TXBUFFER_SIZE;
 
+	//start transmission
 	startTransmit(inst);
 	return TRUE;
 }
@@ -220,6 +306,11 @@ uint8_t UART_SendString(UART_Instance* inst, uint8_t *byte) {
 }
 
 uint16_t UART_GetAvailableBytes(UART_Instance* inst) {
+	//invalid parameter
+	if (!inst) {
+		return 0;
+	}
+
 	if (inst->rxCircWrap) {
 		return UART_RXBUFFER_SIZE - inst->rxCircTail + inst->rxCircHead;
 	} else {
@@ -228,10 +319,18 @@ uint16_t UART_GetAvailableBytes(UART_Instance* inst) {
 }
 
 uint8_t UART_GetByte(UART_Instance* inst) {
-	if (UART_GetAvailableBytes(inst) < 1) {
-		return 0;	//ERROR
+	//invalid parameter
+	if (!inst) {
+		return 0;
 	}
+
+	//check if bytes are available
+	if (UART_GetAvailableBytes(inst) < 1) {
+		return 0;
+	}
+	//retrieve byte
 	uint8_t byte = inst->rxCircBuf[inst->rxCircTail++];
+	//wrap tail if necessary
 	if (inst->rxCircTail >= UART_RXBUFFER_SIZE) {
 		inst->rxCircWrap = FALSE;
 		inst->rxCircTail = 0;
@@ -240,49 +339,74 @@ uint8_t UART_GetByte(UART_Instance* inst) {
 }
 
 uint16_t UART_GetData(UART_Instance* inst, uint16_t len, uint8_t *data) {
+	//invalid parameter
+	if ((!inst) || (!data)) {
+		return FALSE;
+	}
+	
+	//check if enough bytes are available
+	//else adjust length to retreive
 	if (UART_GetAvailableBytes(inst) < len) {
 		len = UART_GetAvailableBytes(inst);
 	}
+
+	//retreive data from buffer
 	for (uint16_t i = 0; i < len; i++) {
 		data[i] = inst->rxCircBuf[(inst->rxCircTail+i)%UART_RXBUFFER_SIZE];
 	}
+	//handle wrap
 	if ((inst->rxCircTail + len) >= UART_RXBUFFER_SIZE) {
 		inst->rxCircWrap = FALSE;
 	}
+	//avance tail index
 	inst->rxCircTail = (inst->rxCircTail + len) % UART_RXBUFFER_SIZE;
+	
+	//start reception
 	startReceive(inst);
 	return len;
 }
 
 static void startTransmit(UART_Instance* inst) {
-	if (HAL_UART_GetState(&(inst->uart)) == HAL_UART_STATE_BUSY_TX) {
+	//check if UART ready to transmit
+	HAL_UART_StateTypeDef state = HAL_UART_GetState(&(inst->uart));
+	if ((state == HAL_UART_STATE_BUSY_TX) || (state == HAL_UART_STATE_BUSY_TX_RX)) {
 		return;
 	}
+	//calculate count of byte to transmit
 	inst->txCount = inst->txCircWrap == FALSE
 			? inst->txCircHead - inst->txCircTail
 			: UART_TXBUFFER_SIZE - inst->txCircTail;
+
+	//transmit data if count > 0
 	if (inst->txCount > 0) {
 		HAL_UART_Transmit_DMA(&(inst->uart), inst->txCircBuf + inst->txCircTail, inst->txCount);
 	}
 }
 
 static void startReceive(UART_Instance* inst) {
-	if (HAL_UART_GetState(&(inst->uart)) == HAL_UART_STATE_BUSY_RX) {
+	//check if uart ready to receive
+	HAL_UART_StateTypeDef state = HAL_UART_GetState(&(inst->uart));
+	if ((state == HAL_UART_STATE_BUSY_RX) || (state == HAL_UART_STATE_BUSY_TX_RX)) {
 		return;
 	}
+	//calculate maximal count of bytes to receive
 	inst->rxCount = inst->rxCircWrap == FALSE
 			? UART_RXBUFFER_SIZE - inst->rxCircHead
 			: inst->rxCircTail - inst->rxCircHead;
 
 	if (inst->rxCount > 0) {
+		//start reception
 		HAL_UART_Receive_DMA(&(inst->uart), inst->rxCircBuf + inst->rxCircHead, inst->rxCount);
+		//clear IDLE interrupt
 		__HAL_UART_CLEAR_IT(&(inst->uart),UART_CLEAR_IDLEF);
+		//enable IDLE interrupt
 		__HAL_UART_ENABLE_IT(&(inst->uart), UART_IT_IDLE);
 	}
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *uart) {
 	UART_Instance *inst = 0;
+	//get instance from table
 	if (uart->Instance == USART1) {
 		inst = instances[UART_1];
 	} else if (uart->Instance == USART2) {
@@ -295,23 +419,28 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *uart) {
 		return;
 	}
 
+	//get count of received byte
 	uint16_t cnt = inst->rxCount - __HAL_DMA_GET_COUNTER(&(inst->rxDma));
 
+	//advance head index and wrap if necessary
 	inst->rxCircHead += cnt;
 	if (inst->rxCircHead >= UART_RXBUFFER_SIZE) {
 		inst->rxCircHead = 0;
 		inst->rxCircWrap = TRUE;
 	}
 
+	//start new reception
 	startReceive(inst);
 }
 
 void HAL_UART_AbortReceiveCpltCallback(UART_HandleTypeDef *uart) {
+	//call uart receive complete callback
 	HAL_UART_RxCpltCallback(uart);
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *uart) {
 	UART_Instance *inst = 0;
+	//get instance from table
 	if (uart->Instance == USART1) {
 		inst = instances[UART_1];
 	} else if (uart->Instance == USART2) {
@@ -324,53 +453,69 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *uart) {
 		return;
 	}
 
+	//get count of transmitted byte
 	uint16_t cnt = inst->txCount - __HAL_DMA_GET_COUNTER(&(inst->txDma));
 
+	//advance tail index and wrap if necessary
 	inst->txCircTail += cnt;
 	if (inst->txCircTail >= UART_TXBUFFER_SIZE) {
 		inst->txCircTail = 0;
 		inst->txCircWrap = FALSE;
 	}
 
+	//start new transmission
 	startTransmit(inst);
 }
 
 void USART1_IRQHandler() {
+	//if instance for uart1 is configured
 	if (instances[UART_1] != 0) {
+		//check for IDLE interrupt
 		if (__HAL_UART_GET_IT(&(instances[UART_1]->uart),UART_IT_IDLE) != FALSE) {
+			//clear interrupt and abort reception
 			__HAL_UART_CLEAR_IT(&(instances[UART_1]->uart),UART_CLEAR_IDLEF);
 			HAL_UART_AbortReceive_IT(&(instances[UART_1]->uart));
-		} else {
-			HAL_UART_IRQHandler(&(instances[UART_1]->uart));
 		}
+		//call HAL interrupt handler
+		HAL_UART_IRQHandler(&(instances[UART_1]->uart));
 	}
 }
 
 void USART2_IRQHandler() {
+	//if instance for uart2 is configured
 	if (instances[UART_2] != 0) {
+		//check for IDLE interrupt
 		if (__HAL_UART_GET_IT(&(instances[UART_2]->uart),UART_IT_IDLE) != FALSE) {
+			//clear interrupt and abort reception
 			__HAL_UART_CLEAR_IT(&(instances[UART_2]->uart),UART_CLEAR_IDLEF);
 			HAL_UART_AbortReceive_IT(&(instances[UART_2]->uart));
 		}
+		//call HAL interrupt handler
+		HAL_UART_IRQHandler(&(instances[UART_2]->uart));
 	}
-	HAL_UART_IRQHandler(&(instances[UART_2]->uart));
 }
 
 void USART4_5_IRQHandler() {
+	//if instance for uart4 is configured
 	if (instances[UART_4] != 0) {
+		//check for IDLE interrupt
 		if (__HAL_UART_GET_IT(&(instances[UART_4]->uart),UART_IT_IDLE) != FALSE) {
+			//clear interrupt and abort reception
 			__HAL_UART_CLEAR_IT(&(instances[UART_4]->uart),UART_CLEAR_IDLEF);
 			HAL_UART_AbortReceive_IT(&(instances[UART_4]->uart));
-		} else {
-			HAL_UART_IRQHandler(&(instances[UART_4]->uart));
 		}
+		//call HAL interrupt handler
+		HAL_UART_IRQHandler(&(instances[UART_4]->uart));
 	}
+	//if instance for uart5 is configured
 	if (instances[UART_5] != 0) {
+		//check for IDLE interrupt
 		if (__HAL_UART_GET_IT(&(instances[UART_5]->uart),UART_IT_IDLE) != FALSE) {
+			//clear interrupt and abort reception
 			__HAL_UART_CLEAR_IT(&(instances[UART_5]->uart),UART_CLEAR_IDLEF);
 			HAL_UART_AbortReceive_IT(&(instances[UART_5]->uart));
-		} else {
-			HAL_UART_IRQHandler(&(instances[UART_5]->uart));
 		}
+		//call HAL interrupt handler
+		HAL_UART_IRQHandler(&(instances[UART_5]->uart));
 	}
 }
--- Firmware/Drivers/User/uart/uart.h
+++ Firmware/Drivers/User/uart/uart.h
@@ -11,6 +11,10 @@
 #define UART_TXBUFFER_SIZE 256
 #define UART_RXBUFFER_SIZE 256
 
+/**
+ * @brief Baud rate definitons
+ * 
+ */
 typedef enum {
 	BAUD_9600 = 9600,
 	BAUD_19200 = 19200,
@@ -19,49 +23,105 @@ typedef enum {
 	BAUD_115200 = 115200
 } UART_BaudRate;
 
+/**
+ * @brief UART Instance structure
+ * 
+ */
 typedef struct {
-	UART_HandleTypeDef uart;
+	UART_HandleTypeDef uart;	//HAL driver UART handle
 
-	DMA_HandleTypeDef rxDma;
-	uint8_t  rxCircBuf[UART_RXBUFFER_SIZE];
-	uint16_t rxCircHead;
-	uint16_t rxCircTail;
-	uint8_t  rxCircWrap;
-	uint16_t rxCount;
+	DMA_HandleTypeDef rxDma;	//HAL driver DMA handle for receiving
+	uint8_t  rxCircBuf[UART_RXBUFFER_SIZE];	//receive buffer
+	uint16_t rxCircHead;	//receive buffer head
+	uint16_t rxCircTail;	//receive buffer tail
+	uint8_t  rxCircWrap;	//Flag if Head got wrapped around
+	uint16_t rxCount;		//count of bytes to receive
 
-	DMA_HandleTypeDef txDma;
-	uint8_t  txCircBuf[UART_RXBUFFER_SIZE];
-	uint16_t txCircHead;
-	uint16_t txCircTail;
-	uint8_t  txCircWrap;
-	uint16_t txCount;
+	DMA_HandleTypeDef txDma;	//HAL driver DMA handle for transmitting
+	uint8_t  txCircBuf[UART_TXBUFFER_SIZE];	//transmit buffer
+	uint16_t txCircHead;	//transmit buffer head
+	uint16_t txCircTail;	//transmit buffer tail
+	uint8_t  txCircWrap;	//Flag if Head got wrapped around
+	uint16_t txCount;		//count of bytes to transmit
 } UART_Instance;
 
+/**
+ * @brief UART Configuration structure
+ * 
+ */
 typedef struct {
-	USART_TypeDef*			uart;
-	DMA_Channel_TypeDef*	txDmaChannel;
-	DMA_Channel_TypeDef*	rxDmaChannel;
-	GPIO_TypeDef* 			txBoard;
-	GPIO_TypeDef* 			rxBoard;
-	uint16_t      			txPin;
-	uint16_t      			rxPin;
-	uint32_t				txAF;
-	uint32_t				rxAF;
-	UART_BaudRate 			baud;
+	USART_TypeDef*			uart;		//USART module to configure
+	DMA_Channel_TypeDef*	txDmaChannel;	//DMA channel used for transmitting
+	DMA_Channel_TypeDef*	rxDmaChannel;	//DMA channel used for receiving
+	GPIO_TypeDef* 			txBoard;	//GPIO board for transmition
+	GPIO_TypeDef* 			rxBoard;	//GPIO board for reception
+	uint16_t      			txPin;		//GPIO pin for transmition
+	uint16_t      			rxPin;		//GPIO pin for reception
+	uint32_t				txAF;		//Alternate function for transmition 
+	uint32_t				rxAF;		//Alternate function for reception
+	UART_BaudRate 			baud;		//baud rate
 } UART_Config;
 
+/**
+ * @brief UART initialisation
+ * 
+ * @param inst empty UART instance
+ * @param conf Configration
+ */
 void UART_Init(UART_Instance* inst, UART_Config* conf);
 
+/**
+ * @brief Send single byte
+ * 
+ * @param inst UART instance
+ * @param byte byte to send
+ * @return uint8_t 1 on success, 0 on failure
+ */
 uint8_t UART_SendByte(UART_Instance* inst, uint8_t byte);
 
+/**
+ * @brief Send array of bytes
+ * 
+ * @param inst UART instance
+ * @param len length of data (count of bytes)
+ * @param data data to send
+ * @return uint8_t 1 on success, 0 on failure
+ */
 uint8_t UART_SendData(UART_Instance* inst, uint16_t len, uint8_t *data);
 
+/**
+ * @brief Send 0-terminated string
+ * 
+ * @param inst UART instance
+ * @param byte string (must be 0-terminated)
+ * @return uint8_t 1 on success, 0 on failure
+ */
 uint8_t UART_SendString(UART_Instance* inst, uint8_t *byte);
 
+/**
+ * @brief Get number of received bytes in buffer
+ * 
+ * @param inst UART instance
+ * @return uint16_t number of bytes available
+ */
 uint16_t UART_GetAvailableBytes(UART_Instance* inst);
 
+/**
+ * @brief Retreive 1 byte from receive buffer
+ * 
+ * @param inst UART instance
+ * @return uint8_t retreived byte
+ */
 uint8_t UART_GetByte(UART_Instance* inst);
 
+/**
+ * @brief Retreive data from receive buffer
+ * 
+ * @param inst UART instance
+ * @param len maximal length of data to retreive
+ * @param data array to store data
+ * @return uint16_t count of retreived bytes
+ */
 uint16_t UART_GetData(UART_Instance* inst, uint16_t len, uint8_t *data);
 
 
