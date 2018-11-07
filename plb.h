351b9a4e6fda644e4bc46517fbd4ceb992f9e76c
S1610306042 (Svyrydova Olga) <s1610306042@students.fh-hagenberg.at>
2018-11-07 13:04:23 +0100

    plb.h

---
+++ plb.h
@@ -0,0 +1,16 @@
+//
+//  plb.h
+//  
+//
+//  Created by Olia Sviridova on 30.09.18.
+//
+
+#ifndef plb_h
+#define plb_h
+
+#include <stdio.h>
+
+void plb_init();
+void plb_protocol();
+
+#endif /* plb_h */
