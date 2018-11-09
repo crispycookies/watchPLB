ee734e197ab0d88a9da01b9428936f01a263e745
Paul Götzinger <paul70079@gmail.com>
2018-11-09 16:27:32 +0100

    documented position interface

--- Firmware/Drivers/Interfaces/position/position.h
+++ Firmware/Drivers/Interfaces/position/position.h
@@ -1,18 +1,37 @@
+/*
+ * position.h
+ *
+ *  Created on: 9.11.2018
+ *      Author: Paul
+ */
+
 #ifndef POSITION_H
 #define POSITION_H
 
 #include <stdint.h>
 
+/**
+ * @brief Position latitude flag
+ * 
+ */
 typedef enum {
 	POS_Latitude_Flag_S = 0,
 	POS_Latitude_Flag_N = 1
 } POS_Latitude_Flag;
 
+/**
+ * @brief Position longitude flag
+ * 
+ */
 typedef enum {
 	POS_Longitude_Flag_W = 0,
 	POS_Longitude_Flag_E = 1
 } POS_Longitude_Flag;
 
+/**
+ * @brief Position timestamp
+ * 
+ */
 typedef struct {
     uint8_t hour;
     uint8_t minute;
@@ -20,18 +39,30 @@ typedef struct {
     uint8_t split;
 } POS_Time;
 
+/**
+ * @brief Position latitude structure
+ * 
+ */
 typedef struct{
 	POS_Latitude_Flag direction;
 	uint16_t degree;
 	double minute;
 } POS_Latitude;
 
+/**
+ * @brief Position latitude structure
+ * 
+ */
 typedef struct{
 	POS_Longitude_Flag direction;
 	uint16_t degree;
 	double minute;
 } POS_Longitude;
 
+/**
+ * @brief position structure
+ * 
+ */
 typedef struct {
     POS_Time time;
 	POS_Latitude latitude;
