26bb6732a1897a7b8472d1ecc3cb4c635043bc11
Paul Götzinger <paul70079@gmail.com>
2018-11-20 13:52:17 +0100

    update for spi driver changes

--- Firmware/App/emergencyCall/emergencyCall.c
+++ Firmware/App/emergencyCall/emergencyCall.c
@@ -26,47 +26,47 @@ static void plbTransmit(uint16_t data);
 static void SetReg(uint8_t addr, uint8_t data);
 
 void EMC_Init(void) {
-    SPI_Init_Struct spi_for_send;
     
     SPI_GPIO_Pair miso;
     miso.bank = GPIOA;
     miso.pin = GPIO_PIN_6;
+
     SPI_GPIO_Pair mosi;
     mosi.bank = GPIOA;
     mosi.pin = GPIO_PIN_7;
+    
     SPI_GPIO_Pair slclk;
     slclk.bank = GPIOA;
     slclk.pin = GPIO_PIN_5;
+    
     SPI_GPIO_Pair cs;
-    cs.bank = GPIOA;
-    cs.pin = GPIO_PIN_4;
+    cs.bank = GPIOC;
+    cs.pin = GPIO_PIN_1;
     
-    SPI_HandleTypeDef spidef;
-    spidef.Instance = SPI1;
-    spidef.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
-    spidef.Init.Mode = SPI_MODE_MASTER;
-    spidef.Init.NSS = SPI_NSS_SOFT;
-    spidef.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    spidef.Init.CRCPolynomial = 7;
-    spidef.Init.TIMode = SPI_TIMODE_DISABLE;
-    spidef.Init.Direction = SPI_DIRECTION_2LINES;
-    spidef.Init.DataSize = SPI_DATASIZE_8BIT;
-    spidef.Init.FirstBit = SPI_FIRSTBIT_LSB;
+    spi.SPI.Instance = SPI1;
+    spi.SPI.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+    spi.SPI.Init.Mode = SPI_MODE_MASTER;
+    spi.SPI.Init.NSS = SPI_NSS_SOFT;
+    spi.SPI.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    spi.SPI.Init.CRCPolynomial = 7;
+    spi.SPI.Init.TIMode = SPI_TIMODE_DISABLE;
+    spi.SPI.Init.Direction = SPI_DIRECTION_2LINES;
+    spi.SPI.Init.DataSize = SPI_DATASIZE_8BIT;
+    spi.SPI.Init.FirstBit = SPI_FIRSTBIT_LSB;
     
-    spidef.Init.CLKPolarity = SPI_POLARITY_LOW;
-    spidef.Init.CLKPhase = SPI_PHASE_1EDGE;
+    spi.SPI.Init.CLKPolarity = SPI_POLARITY_LOW;
+    spi.SPI.Init.CLKPhase = SPI_PHASE_1EDGE;
     
     //not defined
-    //spidef.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-    //spidef.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+    //spi.SPI.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+    //spi.SPI.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
     
-    spi_for_send.CS = cs;
-    spi_for_send.MISO = miso;
-    spi_for_send.MOSI = mosi;
-    spi_for_send.SCLK = slclk;
-    spi_for_send.SPI = &spidef;
+    spi.CS = cs;
+    spi.MISO = miso;
+    spi.MOSI = mosi;
+    spi.SCLK = slclk;
     
-    SPI_Init(&spi_for_send);
+    SPI_Init(&spi);
 
     //init plb with spi
     PLB_Init(&plb, plbTransmit);
