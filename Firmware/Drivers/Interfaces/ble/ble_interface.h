c3e494cfb74646a4ad9c8cf3acaeb1feac99b010
S1610306008 (Egger Tobias) <s1610306008@students.fh-hagenberg.at>
2018-11-20 10:25:24 +0100

    Update ble_interface.h

--- Firmware/Drivers/Interfaces/ble/ble_interface.h
+++ Firmware/Drivers/Interfaces/ble/ble_interface.h
@@ -13,18 +13,13 @@
  *
  */
 
-typedef struct {
-	uint8_t mac;
-	uint8_t ip;
-} ble_interface_dev_id;
-
 void ble_interface_init();
 void ble_interface_send(uint8_t * tx_buffer, uint8_t tx_buffer_length);
 uint16_t ble_interface_get_buffer_length();
 uint16_t ble_interface_get_buffer(uint8_t *rx_buffer, uint8_t rx_buffer_length);
 void ble_interface_deinit();
 
-void ble_interface_connect(const ble_interface_dev_id * dev_id);
-void ble_interface_disconnect(const ble_interface_dev_id * dev_id);
+void ble_interface_connect();
+void ble_interface_disconnect();
 void ble_interface_set_name(const uint8_t * ble_name, const uint8_t ble_name_len);
 void ble_interface_advertize(const bool ble_advertize);
