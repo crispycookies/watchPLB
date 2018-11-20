ae2563ecd808f6b1602fd987e7e3aac8543b2481
S1610306008 (Egger Tobias) <s1610306008@students.fh-hagenberg.at>
2018-11-20 09:59:57 +0100

    Update ble_interface.h

--- Firmware/Drivers/Interfaces/ble/ble_interface.h
+++ Firmware/Drivers/Interfaces/ble/ble_interface.h
@@ -24,12 +24,7 @@ uint16_t ble_interface_get_buffer_length();
 uint16_t ble_interface_get_buffer(uint8_t *rx_buffer, uint8_t rx_buffer_length);
 void ble_interface_deinit();
 
-void ble_interface_sleep();
-void ble_interface_awake();
-
 void ble_interface_connect(const ble_interface_dev_id * dev_id);
 void ble_interface_disconnect(const ble_interface_dev_id * dev_id);
-
-void ble_interface_allow_connection();
-bool ble_interface_is_connection_availiable();
-ble_interface_dev_id * ble_interface_get_availiable_conections();
+void ble_interface_set_name(const uint8_t * ble_name, const uint8_t ble_name_len);
+void ble_interface_advertize(const bool ble_advertize);
