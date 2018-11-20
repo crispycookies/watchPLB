8493aabf38c72a5ea5b8672707dc2c4e49469f9c
S1610306008 (Egger Tobias) <s1610306008@students.fh-hagenberg.at>
2018-11-20 09:58:08 +0100

    Update ble_interface.c

--- Firmware/Drivers/Interfaces/ble/ble_interface.c
+++ Firmware/Drivers/Interfaces/ble/ble_interface.c
@@ -4,15 +4,92 @@
  *  Created on: 02.10.2018
  *      Author: tobi
  */
+#include <stdlib.h>
 
 #include "ble_interface.h"
+#include "crc8.h"
 
 #include "uart.h"
 
 #define UART_START_SEQ 0xAA
+/*@brief BLEDK3 CMDs*/
+/*@brief COMMON 1*/
+#define RL_INFO 0x01
+#define RESET 0x02
+#define R_BLEDK3 0x03
+#define R_ADC_Value 0x04
+#define INTO_SHTDWN_MODE 0x05
+#define DEBUG_CMD 0x06
+#define W_DEV_NAME 0x08
+#define R_DEV_NAME 0x07
+#define ERASE_ALL_PAIRED_INFO 0x09
+#define R_PAIRING_MODE_STNG 0x0A
+#define W_PAIRING_MODE_STNG 0x0B
+#define R_ALL_PAIRED_INFO 0x0C
+#define DELETE_PAIRED_DEV 0x0D
+#define DIO_CTRL 0x0E
+#define PWM_CTRL 0x0F
+
+/*@brief GAP*/
+#define R_RSSI_VALUE 0x10
+#define W_ADV_DATA 0x11
+#define W_SCN_RES_DATA 0x12
+#define SET_ADVERTISING_PARAMETER 0x13
+#define SET_SCAN_PARAMETER 0x15
+#define SET_SCAN_ENABLE 0x16
+#define LE_CREATE_CONN 0x17
+#define LE_CREATE_CONN_CANCEL 0x18
+#define CONN_PARAM_UPDATE_REQ 0x19
+#define DC 0x1B
+#define SET_ADV_ENABLE 0x1C
+#define R_REMOTE_DEV_NAME 0x1F
+
+/*@brief GATT_Client*/
+#define DISCO_ALL_PRIM_SERVICES 0x30
+#define DISCO_SPECIFIC_PRIM_SERVICE_CHARACTERISTICS 0x31
+#define R_CHARACTERISTIC_VALUE 0x32
+#define R_USING_CHARACTERISTIC_UID 0x33
+#define W_CHARACTERISTIC_VALUE 0x34
+#define ENABLE_TRANSPARENT 0x35
+
+/*@brief GATT_Server*/
+#define SEND_CHARACTERISTIC_VALUE 0x38
+#define UPDATE_CHARACTERISTIC_VALUE 0x39
+#define R_LOCAL_CHARACTERISTICS_VALUE0x3A
+#define R_LOCAL_ALL_PRIM_SERVICES 0x3B
+#define R_LOCAL_SPECIFIC_PRIMARY_SERVICE 0x3C
+#define SEND_WRITE_RESPONSE 0x3D
+
+/*@brief GATT_Transparent*/
+#define SEND_TRANSPARENT_DATA 0x3F
+
+/*@brief Pairing*/
+#define PASSKYE_ENTRY_RES 0x40
+#define USER_CNFRM_RES 0x41
+#define PAIRING_REQUEST 0x42
+
+/*@brief COMMON 2*/
+#define LEAVE_CFG_MODE 0x52
+
+/*@brief BLEDK3 ERROR*/
+#define SUCCEED 0x00
+#define FAIL_LWR 0x01
+#define FAIL_UPR 0xFF
+
+/*@brief ENABLE DISABLE*/
+#define DEF_ENABLE_TRANSPARENT_PARAM 0x01
+#define DEF_DISABLE_TRANSPARENT_PARAM 0x00
+
+static UART_Config conf;
+static UART_Instance inst;
+
+#define maxbuffer 255
+static uint8_t * send_buffer = 0;
+static uint8_t * rec_buffer = 0;
+static uint8_t connhdl = 0xFF;
 
 static uint8_t ble_crc_hack(uint8_t * data, uint8_t data_len) {
-	return 0;
+	return __crc8(data, data_len);
 }
 
 static void ble_mask(uint8_t command, uint8_t * data, uint8_t data_length,
@@ -23,21 +100,35 @@ static void ble_mask(uint8_t command, uint8_t * data, uint8_t data_length,
 	result[0] = UART_START_SEQ;
 	result[1] = data_length;
 	result[2] = command;
+
 	int i = 0;
+
 	for (; i < data_length; i++) {
 		result[i + 3] = data[i];
 	}
 
-	//TODO CRC for CS
-	result[i] = ble_crc_hack(result, data_length+3);
+	result[i] = ble_crc_hack(result, data_length + 3);
+}
+static void zero_arr(){
+	for(int i = 0; i < maxbuffer; i++){
+		send_buffer[i] = 0;
+	}
+}
+static bool ble_write(uint8_t command, uint8_t * data, uint8_t data_length){
+	zero_arr();
+	ble_mask(command, data, data_length, send_buffer, maxbuffer);
+	UART_SendData(&inst,data_length+4,send_buffer);
+	//TODO Check if it Returns only Byte
+	UART_GetByte(&inst);
+	if(UART_GetByte(&inst)!=SUCCEED) return false;
+	return true;;
 }
 
 void ble_interface_init() {
 
 	//READY
-
-	UART_Config conf;
-	UART_Instance inst;
+	send_buffer= malloc(maxbuffer);
+	rec_buffer = malloc(maxbuffer);
 
 	conf.baud = BAUD_115200;
 	conf.uart = USART1;
@@ -51,38 +142,53 @@ void ble_interface_init() {
 	UART_Init(&inst, &conf);
 }
 void ble_interface_send(uint8_t * tx_buffer, uint8_t tx_buffer_length) {
-
-}
-uint16_t ble_interface_get_buffer_length() {
-	return 0;
-}
-uint16_t ble_interface_get_buffer(uint8_t *rx_buffer, uint8_t rx_buffer_length) {
-	return 0;
-}
-void ble_interface_deinit() {
-
+	//TODO Get To Transparent Mode
+	uint8_t cmd_assembled[3] = {connhdl, DEF_ENABLE_TRANSPARENT_PARAM, 0};
+	ble_write(ENABLE_TRANSPARENT, cmd_assembled, 3);
+	//TODO Send Data
+	ble_write(SEND_TRANSPARENT_DATA, tx_buffer, tx_buffer_length);
+	//TODO Get To Transparent Mode
+	cmd_assembled[1] = DEF_DISABLE_TRANSPARENT_PARAM;
+	ble_write(ENABLE_TRANSPARENT, cmd_assembled, 3 );
 }
 
-void ble_interface_sleep() {
-
+uint16_t ble_interface_get_buffer_length(){
+	uint16_t bytes = 0;
+	if((bytes=UART_GetAvailableBytes(&inst))==0 || bytes <= 3) return 0;
+	return bytes - 3;
+};
+uint16_t ble_interface_get_buffer(uint8_t *rx_buffer, uint8_t rx_buffer_length){
+	if(rx_buffer_length +3 >= maxbuffer) return 0;
+	uint16_t len = UART_GetData(&inst, rx_buffer_length+3, rec_buffer)-3;
+	for(int i = 2; i < rx_buffer_length; i++){
+		rx_buffer[i] = rec_buffer[i];
+	}
+	return len;
 }
-void ble_interface_awake() {
 
+void ble_interface_connect(const ble_interface_dev_id * dev_id){
+	
+}
+void ble_interface_disconnect(const ble_interface_dev_id * dev_id){
+	uint8_t buffer = 0;
+	ble_write(DC, &buffer, 1);
 }
 
-void ble_interface_connect(const ble_interface_dev_id * dev_id) {
-
-}
-void ble_interface_disconnect(const ble_interface_dev_id * dev_id) {
-
+void ble_interface_deinit(){
+	free(send_buffer);
+	free(rec_buffer);
 }
 
-void ble_interface_allow_connection() {
-
+void ble_interface_set_name(const uint8_t * ble_name, const uint8_t ble_name_len){
+	ble_write(W_DEV_NAME, ble_name, ble_name_len);
 }
-bool ble_interface_is_connection_availiable() {
-	return false;
-}
-ble_interface_dev_id * ble_interface_get_availiable_conections() {
-
+void ble_interface_advertize(const bool ble_advertize){
+	if(!ble_advertize){
+		uint8_t adv_ = 0x00;
+		ble_write(SET_ADV_ENABLE, &adv,1);
+	}
+	else{
+		uint8_t adv_ = 0x81;
+		ble_write(SET_ADV_ENABLE, &adv,1);
+	}
 }
