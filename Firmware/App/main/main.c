61f0534a05994c8ad0a14ad522e419d94f20cadb
Paul Götzinger <paul70079@gmail.com>
2018-12-01 02:02:37 +0100

    readded necessary initialization

--- Firmware/App/main/main.c
+++ Firmware/App/main/main.c
@@ -66,6 +66,18 @@ void _Error_Handler(char *file, int line);
   * @retval None
   */
 int main(void) {
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	__HAL_RCC_PWR_CLK_ENABLE();
+
+	/* System interrupt init*/
+	/* SVC_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SVC_IRQn, 0, 0);
+	/* PendSV_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+	HAL_Init();
 	SystemClock_Config();
 	LOG_Init();
 	
